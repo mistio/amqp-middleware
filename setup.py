3bb254c6b5c83463433e7311fb9af2d2a8ce30f8
Christos Pollalis <pollalis.ch@gmail.com>
2017-09-27 15:39:27 +0300

    Bump version

--- setup.py
+++ setup.py
@@ -1,22 +1,22 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 
 setup(
     name='amqp-middleware',
-    version='1.0',
+    version='1.1.2',
     description='AMQP Consumer-Publisher Middleware',
     url='https://github.com/mistio/amqp-middleware',
     license='Apache',
     author='Chris Pollalis',
     author_email='cpollalis@mist.io',
-    packages=['amqpconsumer'],
+    packages=find_packages(),
     entry_points={
         'console_scripts': [
             'amqp-middleware = amqpconsumer.components.agent:bootstrap',
         ],
     },
     dependency_links=[
-        'git+https://github.com/pchristos/amqp-middleware.git@master#egg=amqp-middleware',
+        'git+https://github.com/mistio/amqp-middleware.git@master#egg=amqp-middleware',
     ],
     install_requires=[
         'pika>=0.9.14',
